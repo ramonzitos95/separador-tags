797c6ae181d5b9b862ccbc60db3ef16940147a58
Ramon Silva <ramonss.bque@gmail.com>
2023-11-24 01:31:56 -0300

    Separador de tags youtube em C#

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+try
+{
+    Console.WriteLine("Informe as tags");
+    var tags = Console.ReadLine();
+    var tagsSep = tags?.Split(',') ?? null;
+
+    if(tagsSep is null)
+        Console.WriteLine("Tags não informadas");
+    else 
+    {
+        tagsSep = tagsSep.Select(tag => tag + Environment.NewLine).ToArray();
+
+        Console.WriteLine("Tags com quebra de linha:");
+        foreach (var tag in tagsSep)
+        {
+            Console.WriteLine(tag);
+        }
+    }
+} 
+catch(Exception ex) {
+
+}
